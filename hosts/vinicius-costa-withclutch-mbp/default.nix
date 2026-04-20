da9ede1e3b6fbd28b41a235342a46d553377ee6b
Vinicius Costa <viniciusrdacosta@gmail.com>
2026-04-20 16:00:40 -0300

    feat(host): vinicius-costa-withclutch-mbp entrypoint

---
+++ hosts/vinicius-costa-withclutch-mbp/default.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+{
+  imports = [
+    ../../modules/cli.nix
+    ../../modules/shell.nix
+    ../../modules/git.nix
+    ../../modules/editors.nix
+    ../../modules/languages.nix
+    ../../modules/containers.nix
+    ../../modules/ssh.nix
+    ../../modules/claude-code.nix
+    ../../modules/secrets.nix
+  ];
+
+  home.username = "vinicius.costa";
+  home.homeDirectory = "/Users/vinicius.costa";
+  home.stateVersion = "24.11";
+
+  programs.home-manager.enable = true;
+  fonts.fontconfig.enable = true;
+}

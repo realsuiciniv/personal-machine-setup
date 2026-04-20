d5cd33ae3189de10b99ee6aa56de45d6f41dd16d
Vinicius Costa <viniciusrdacosta@gmail.com>
2026-04-20 16:02:32 -0300

    feat(modules): ssh — route auth through 1Password SSH agent

---
+++ modules/ssh.nix
@@ -0,0 +1,10 @@
+{ config, lib, ... }:
+{
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host *
+        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    '';
+  };
+}

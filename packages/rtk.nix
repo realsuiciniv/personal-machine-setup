f34b6f95e0a9f174e0d3845ea4c5f31f8431c660
Vinicius Costa <viniciusrdacosta@gmail.com>
2026-06-01 10:06:09 -0300

    chore(deps): bump rtk 0.38.0 -> 0.42.0

--- packages/rtk.nix
+++ packages/rtk.nix
@@ -1,10 +1,10 @@
 { stdenv, fetchurl }:
 stdenv.mkDerivation {
   pname = "rtk";
-  version = "0.38.0";
+  version = "0.42.0";
   src = fetchurl {
-    url = "https://github.com/rtk-ai/rtk/releases/download/v0.38.0/rtk-aarch64-apple-darwin.tar.gz";
-    sha256 = "0ypr50wwfvb5h9wbz8rqx7j3agr3d6aimsc8mpd1sr027p2ci5iq";
+    url = "https://github.com/rtk-ai/rtk/releases/download/v0.42.0/rtk-aarch64-apple-darwin.tar.gz";
+    sha256 = "092p323jj1lqrx2fm5ajqkgcin96mgka1azb5qsb706z3k8rrp6d";
   };
   dontUnpack = true;
   installPhase = ''
